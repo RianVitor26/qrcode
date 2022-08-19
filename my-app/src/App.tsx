c3d5edc8f4403e846c5dea844b42e109a4ee4644
RianVitor26 <rianvitorlhe@gmail.com>
2022-08-19 05:03:56 -0300

    feat: adicionando o value do input ao value do qrcode através do usestate

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,20 @@
 import { GlobalStyle } from './globalStyle'
 import * as C from './style'
 import QRCode from 'react-qr-code'
+import { useState } from 'react'
 
 function App() {
+
+  const [link, setLink] = useState('')
+
+  function handleQrCode(e: any) {
+    setLink(e.target.value)
+  }
+
   return (
     <C.Container>
-      <QRCode value='www.google.com'/>
-      <input type="text" placeholder='Digite um link'/>
+      <QRCode value={link} />
+      <input onChange={(e) => handleQrCode(e)} type="text" placeholder='Digite um link'/>
       <GlobalStyle />
     </C.Container>
   )
